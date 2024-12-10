6bfc8d35ed9e2b33516f0d8d79c6038810ac5986
julia <me@juliapaci.com>
2024-12-10 22:10:45 +1100

    cleanup and better object "generics"
    using an object type enum instead of opaque ptr with size

--- src/engine/animation.c
+++ src/engine/animation.c
@@ -1,46 +1,66 @@
 #include "animation.h"
 
-// Animator
+void animation_init(Animation *animation, Renderer *renderer) {
+    *animation = (Animation){.renderer = renderer, 0};
 
-void animator_init(Animator *animator) {
-    *animator = (Animator){0};
-
-    arrsetcap(animator->objects, 10);
+    arrsetcap(animation->objects, 10);
 }
 
-void animator_free(Animator *animator) {
-    arrfree(animator->objects);
+// NOTE: does not free animation.renderer
+void animation_free(Animation *animation) {
+    arrfree(animation->objects);
 }
 
-void animator_play(Animator *animator) {
-    if(animator->time == animator->duration)
+void animation_render_object(Animation *animation, Object *object) {
+    switch(object->kind) {
+        case OBJECT_RECTANGLE: {
+            render_draw_rectangle(animation->renderer, *(Rectangle *)object->object, 0);
+        } break;
+
+        case OBJECT_CUBE: {
+            render_draw_cube(animation->renderer, *(Cube *)object->object, 0);
+        } break;
+
+        case OBJECT_CIRCLE: {
+            render_draw_circle(animation->renderer, *(Circle *)object->object, 0);
+        } break;
+
+        default: break;
+    }
+}
+
+void animation_play(Animation *animation) {
+    if(animation->time == animation->duration)
         return;
-    animator->time++;
+    animation->time++;
 
-    for(size_t i = 0; i < arrlen(animator->objects); i++)
-        animator_object_play(&animator->objects[i], animator->time);
+    for(size_t i = 0; i < arrlen(animation->objects); i++)
+        animation_object_play(animation, &animation->objects[i], animation->time);
 }
 
-void animator_object_play(Object *object, float time) {
-    size_t *actions = animator_current_actions(object, time);
+void animation_object_play(Animation *animation, Object *object, float time) {
+    size_t *actions = animation_active_actions(object, time);
 
     for(size_t i = 0; i < actions[0]; i++)
-        object->actions[i].mutate_object(object);
+        object->actions[i].mutate_object(
+            object,
+            (time - object->actions[i].start)/(object->actions[i].end - object->actions[i].start)
+        );
 
     free(actions);
 
-    // render object (object->actions[0])
+    animation_render_object(animation, object);
 }
 
-void animator_object_add(
-    Animator *animator,
+void animation_object_add(
+    Animation *animation,
     void *actor,
-    const size_t actor_size,
+    const ObjectKind kind,
     float time
 ) {
     Object object = (Object){
         .object = actor,
-        .object_size = actor_size
+        .kind = kind
     };
     arrsetcap(object.actions, 3);
 
@@ -52,36 +72,35 @@ void animator_object_add(
     };
     arrput(object.actions, object_action);
 
-    const size_t index = animator_object_place(animator, time);
-    arrins(animator->objects, index, object);
+    const size_t index = animation_object_place(animation, time);
+    arrins(animation->objects, index, object);
 }
 
-void animator_object_action_add(Object *object, Action action) {
-    const size_t index = animator_action_place(object, action.start);
+void animation_object_action_add(Object *object, Action action) {
+    const size_t index = animation_action_place(object, action.start);
     arrins(object->actions, index, action);
 }
 
-size_t animator_object_place(Animator *animator, float time) {
+size_t animation_object_place(Animation *animation, float time) {
     size_t l = 0;
-    size_t r = arrlen(animator->objects) - 1;
+    size_t r = arrlen(animation->objects) - 1;
     while(l < r) {
         const size_t m = (r - l)/2;
-        const float object = animator->objects[m].actions[0].start;
+        const float object = animation->objects[m].actions[0].start;
 
         if(time > object)
             l = m;
         else if(time < object)
             r = m;
         else if(time == object) {
-            l = m;
-            break;
+            return m;
         }
     }
 
     return l;
 }
 
-size_t animator_action_place(Object *object, float time) {
+size_t animation_action_place(Object *object, float time) {
     size_t l = 0;
     size_t r = arrlen(object->actions) - 1;
     while(l < r) {
@@ -93,8 +112,7 @@ size_t animator_action_place(Object *object, float time) {
         else if(time < action)
             r = m;
         else if(time == action) {
-            l = m;
-            break;
+            return m;
         }
     };
 
@@ -103,12 +121,13 @@ size_t animator_action_place(Object *object, float time) {
 
 // needs to be freed
 // first size_t is the amount of elements
-size_t *animator_current_actions(Object *object, float time) {
-    const size_t start = animator_action_place(object, time);
-    size_t end = start;
+size_t *animation_active_actions(Object *object, float time) {
+    const size_t start = animation_action_place(object, time);
+    size_t end;
 
     size_t buffer[arrlen(object->actions)];
-    for(;
+    buffer[0] = start;
+    for(end = start + 1;
         end < arrlen(object->actions) &&
         time >= object->actions[end].start &&
         time <= object->actions[end].end;
@@ -118,6 +137,14 @@ size_t *animator_current_actions(Object *object, float time) {
 
     size_t *elements = malloc((end-start) * sizeof(size_t));
     elements[0] = end-start;
-    memcpy(&elements[1], buffer, end-start);
+    memcpy(elements + 1, buffer, end-start);
     return elements;
 }
+
+#ifdef ANIMATION_UTILITIES
+
+float lerp_f32(float a, float b, float t) {
+    return a * (1.f - t) + (b * t);
+}
+
+#endif // ANIMATION_UTILITIES
--- src/engine/animation.h
+++ src/engine/animation.h
@@ -1,23 +1,33 @@
 #ifndef __ANIMATION_H__
 #define __ANIMATION_H__
+// define `ANIMATION_UTILITIES` for utility functions like `lerp`
 
 // NOTE: will probably reimplement in another language for better generic support since void *, macros, and metaprogramming is annoying to use
+
+#include <engine/renderer.h>
 
 #include <stb/stb_ds.h>
 
 struct Object;
 
-// time is in ms
-// TODO: maybe should be in frames instead of time?
+// all timing is in 60 frames (1 (float) = 60 (frames))
 
 typedef struct {
     float start;
     float end;
 
     // function to act upon the object
-    void (*mutate_object)(struct Object *object);
+    // `t` should be normalised [0, 1]
+    void (*mutate_object)(struct Object *object, float t);
 } Action;
 
+typedef enum {
+    OBJECT_RECTANGLE,
+    OBJECT_CUBE,
+    OBJECT_CIRCLE
+} ObjectKind;
+
+// TODO: maybe an ecs for transform and other components
 // an animated entity
 typedef struct Object {
     // actions will always be sorted by their start time
@@ -27,30 +37,38 @@ typedef struct Object {
     // actor
     // can be any renderer shape like rectangle or something
     void *object;
-    /* const */ size_t object_size;
+    /* const */ ObjectKind kind; // type of object
 } Object;
 
 // animation context
 typedef struct {
-    float time;
-    float duration; // duration of animation
+    Renderer *renderer; // rendering context to draw stuff with
 
-    Object *objects; // stb dynamic array
-} Animator;
+    float time;         // current time
+    float duration;     // duration of animation
 
-// Animator
-void animator_init(Animator *animator);
-void animator_free(Animator *animator);
+    Object *objects;    // stb dynamic array
+} Animation;
 
-void animator_play(Animator *animator); // plays an animation frame
-void animator_object_play(Object *object, float time); // plays animation frame
+// animation
+void animation_init(Animation *animation, Renderer *renderer);
+void animation_free(Animation *animation);
 
-void animator_object_add(Animator *animator, void *object, const size_t object_size, float time);
-void animator_object_action_add(Object *object, Action action);
+void animation_render_object(Animation *animation, Object *object);
+void animation_play(Animation *animation); // plays an animation frame
+void animation_object_play(Animation *animation, Object *object, float time); // plays animation frame
+
+void animation_object_add(Animation *animation, void *object, const ObjectKind kind, float time);
+void animation_object_action_add(Object *object, Action action);
 
 // binary searches for placements for sorted actors
-size_t animator_object_place(Animator *animator, float time); // index of `animator`'s objects which `time` should sort into
-size_t animator_action_place(Object *object, float time); // index of `object`'s actions which `time` should sort into
-size_t *animator_current_actions(Object *object, float time); // index of the first action which `time` lies within
+size_t animation_object_place(Animation *animation, float time); // index of `animation`'s objects which `time` should sort into
+size_t animation_action_place(Object *object, float time); // index of `object`'s actions which `time` should sort into
+size_t *animation_active_actions(Object *object, float time); // index of the first action which `time` lies within
+
+// animation utilities
+#ifdef ANIMATION_UTILITIES
+float lerp_f32(float a, float b, float t);
+#endif // ANIMATION_UTILITIES
 
 #endif // __ANIMATION_H__
